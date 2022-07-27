a4bdbef4cd2ff07f7da98cb95a07938e8629031a
Chirag Kalwani <75169388+chirag-kalwani@users.noreply.github.com>
2022-07-27 23:53:51 +0530

    Removed unwanted files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React, {Component} from 'react';
 import Navbar from "./Components/Navbar";
 import News from "./Components/News";
-import './Components/News.css'
 class App extends Component {
     render() {
         return (
--- src/Components/News.js
+++ src/Components/News.js
@@ -2,55 +2,45 @@ import React, {Component} from 'react';
 import NewsItem from "./NewsItem";
 
 class News extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
             articles: [],
             loading: false,
-            page: 1
+            page: 1,
+            totalResults: 0
         }
     }
 
-    maxPage = 2;
-
-    async componentDidMount() { // run after render
-        // console.log('cdm');
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=537546ef4b964485a77197786029fec9&pageSize=15&page=${this.state.page}`;
+    async fetchData(page) {
+        let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=537546ef4b964485a77197786029fec9&pageSize=5&page=${page}`;
         let data = await fetch(url);
         let parsedData = await data.json();
-        console.log(parsedData);
-        this.maxPage = parsedData.length;
-        // console.log(parsedData);
-        this.setState({articles: parsedData.articles, loading: true, totalResults: parsedData.totalResults});
+        this.setState({
+            articles: parsedData.articles,
+            loading: true,
+            totalResults: parsedData.totalResults
+        });
+    }
+
+    async componentDidMount() { // run after render
+        await this.fetchData(1);
     }
 
     prevPage = async () => {
         let pageNo = this.state.page - 1;
         this.setState({page: pageNo});
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=537546ef4b964485a77197786029fec9&pageSize=15&page=${this.state.page}`;
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({articles: parsedData.articles, loading: true, totalResults: parsedData.totalResults});
+        await this.fetchData(pageNo);
         window.scrollTo(0, 0);
     };
-    nextPage = async (e) => {
-        console.log("Next Page Clicked");
-        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 15)) {
-            console.log(e);
-            return;
-        }
+    nextPage = async () => {
         let pageNo = this.state.page + 1;
         this.setState({page: pageNo});
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=537546ef4b964485a77197786029fec9&pageSize=15&page=${this.state.page}`;
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({articles: parsedData.articles, loading: true, totalResults: parsedData.totalResults});
+        await this.fetchData(pageNo);
         window.scrollTo(0, 0);
     };
 
     render() {
-        // console.log('render');
         return (
             <>
                 <div className='container'>
@@ -74,7 +64,8 @@ class News extends Component {
                         <button disabled={this.state.page <= 1} type="button" onClick={this.prevPage}
                                 className="btn btn-dark">&larr; Prev
                         </button>
-                        <button type="button" onClick={this.nextPage}
+                        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 5)} type="button"
+                                onClick={this.nextPage}
                                 className="btn btn-dark">Next &rarr;</button>
                     </div>
                 </div>
