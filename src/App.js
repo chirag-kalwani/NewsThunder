19d6d92c04ea4494c71657292fc23032a8e1e9b2
Chirag Kalwani <75169388+chirag-kalwani@users.noreply.github.com>
2022-07-28 00:24:15 +0530

    Spinner added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import React, {Component} from 'react';
 import Navbar from "./Components/Navbar";
 import News from "./Components/News";
+
 class App extends Component {
     render() {
         return (
             <>
                 <Navbar title="News Thunder"/>
-                <News/>
+                <News pageSize={5}/>
             </>
         );
     }
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,32 +1,31 @@
 import React, {Component} from 'react';
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 class News extends Component {
     constructor(props) {
         super(props);
         this.state = {
             articles: [],
-            loading: false,
+            loading: true,
             page: 1,
             totalResults: 0
         }
     }
-
     async fetchData(page) {
-        let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=537546ef4b964485a77197786029fec9&pageSize=5&page=${page}`;
+        let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=537546ef4b964485a77197786029fec9&pageSize=${this.props.pageSize}&page=${page}`;
+        this.setState({loading: true});
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
             articles: parsedData.articles,
-            loading: true,
+            loading: false,
             totalResults: parsedData.totalResults
         });
     }
-
     async componentDidMount() { // run after render
         await this.fetchData(1);
     }
-
     prevPage = async () => {
         let pageNo = this.state.page - 1;
         this.setState({page: pageNo});
@@ -45,13 +44,10 @@ class News extends Component {
             <>
                 <div className='container'>
                     <h2>News Thunder - Top News headlines</h2>
-                    {!(this.state.loading) && <div className="d-flex justify-content-center">
-                        <div className="spinner-border" role="status">
-                            <span className="visually-hidden">Loading...</span>
-                        </div>
-                    </div>}
+                    {this.state.loading && <Spinner/>}
+                    {/* This is loop insie jsx to create as many Newsitems as we want */}
                     <div className="row">
-                        {this.state.articles.map((ele) => {
+                        {!this.state.loading && this.state.articles.map((ele) => {
                             return (<div className="col-md-4 my-3" key={ele.url !== null ? ele.url : ' '}>
                                 <NewsItem title={ele.title !== null ? ele.title : ' '}
                                           description={ele.description !== null ? ele.description : ' '}
@@ -60,13 +56,16 @@ class News extends Component {
                             </div>)
                         })}
                     </div>
+                    {/* Buttons to navigate next and prev page */}
                     <div className="d-flex justify-content-between">
                         <button disabled={this.state.page <= 1} type="button" onClick={this.prevPage}
                                 className="btn btn-dark">&larr; Prev
                         </button>
-                        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 5)} type="button"
-                                onClick={this.nextPage}
-                                className="btn btn-dark">Next &rarr;</button>
+                        <button
+                            disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)}
+                            type="button"
+                            onClick={this.nextPage}
+                            className="btn btn-dark">Next &rarr;</button>
                     </div>
                 </div>
             </>
